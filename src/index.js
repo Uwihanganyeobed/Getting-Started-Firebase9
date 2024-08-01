ec168a182b8e71d5dd3a7e927a40e4734b260af5
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-08-01 18:43:51 +0200

    laat commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+import {initializeApp} from 'firebase/app'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
    apiKey: "AIzaSyDpd7whHrN82tg2A60EGgWOKW-6cmswqeg",
@@ -8,3 +11,5 @@ const firebaseConfig = {
    appId: "1:1049726743728:web:7fdd92ec1ede40bbedf775",
    measurementId: "G-948TQNM8ER"
  };
+
+initializeApp(firebaseConfig)
